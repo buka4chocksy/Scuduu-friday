d0158a2fece3672fab447cd8aa323702e45b4463
ochoko chukwuebuka peter <buka4chocksy@gmail.com>
2021-03-05 14:16:44 +0100

    refactored code and added active state for side tab

--- src/screens/task/task.js
+++ src/screens/task/task.js
@@ -1,27 +1,66 @@
 import React, {useState} from 'react';
-import {View, Text, TouchableOpacity, Image} from 'react-native';
+import {View, Text, TouchableOpacity, Image, ScrollView} from 'react-native';
 import Style from './taskStyle';
 import Bugger from '../../assets/bugger.png';
 import Bell from '../../assets/bell.png';
-
 import bed from '../../assets/bed.png';
+import activeBed from '../../assets/activebed.png';
 import chair from '../../assets/chair.png';
+import activechair from '../../assets/activechair.png';
 import bathtop from '../../assets/bathtop.png';
+import activebathtop from '../../assets/bathtopactive.png';
 import alarm from '../../assets/alarm.png';
+import activealarm from '../../assets/alarmactive.png';
 import micro from '../../assets/micro.png';
-import palor from '../../assets/palor.png';
+import activemicro from '../../assets/kitchenactive.png';
 import cross from '../../assets/cross.png';
 import switchLogo from '../../assets/switch.png';
+import Device from '../../components/device';
+
 const tabArray = [
-  {icon: bed, name: 'room'},
-  {icon: chair, name: 'palour'},
-  {icon: micro, name: 'kitchen'},
-  {icon: bathtop, name: 'bathroom'},
-  {icon: alarm, name: 'alarm'},
+  {icon: bed, name: 'Room', activeIcon: activeBed},
+  {icon: chair, name: 'Palour', activeIcon: activechair},
+  {icon: micro, name: 'Kitchen', activeIcon: activemicro},
+  {icon: bathtop, name: 'Bathroom', activeIcon: activebathtop},
+  {icon: alarm, name: 'Alarm', activeIcon: activealarm},
+];
+
+const switchArray = [
+  {
+    icon: switchLogo,
+    mainName: 'Air Conditioner',
+    devicename: 'LG AC29010',
+    time: '3hrs ago',
+  },
+  {
+    icon: switchLogo,
+    mainName: 'Smart Tv',
+    devicename: 'Samsung UHD 5699',
+    time: '3hrs ago',
+  },
+  {
+    icon: switchLogo,
+    mainName: 'Standing Fan',
+    devicename: 'Binatone SF2312',
+    time: '3hrs ago',
+  },
+  {
+    icon: switchLogo,
+    mainName: 'Macbook Pro',
+    devicename: 'Apple 2020 Macbook Pro 13.3',
+    time: '3hrs ago',
+  },
+  {
+    icon: switchLogo,
+    mainName: 'Onyx Studio 6',
+    devicename: 'H/K Onyx Studio 6',
+    time: '3hrs ago',
+  },
 ];
 
 const Task = () => {
   const [active, setActive] = useState('');
+
   return (
     <View style={[Style.Container, Style.rowDirection]}>
       <View style={Style.mainView}>
@@ -35,24 +74,27 @@ const Task = () => {
           <Text style={Style.greetingText}>Good Afternoon</Text>
           <Text style={Style.nameStyle}>Franklin</Text>
         </View>
-    <View style={Style.connectedWrapper}>
-    <Text style={Style.greetingText}>Connected Device</Text>
-    <View style={[Style.rowDirection , Style.switchSWrappertyle]}>
-    <View >
-    <Text style={Style.devicetext}>Air Conditioner</Text>
-    <Text style={Style.deviceSubtext}>LG AC29010</Text>
-    <Text style={Style.deviceSubtext2}>Last Uptime</Text>
-    </View>
-    <View >
-    <TouchableOpacity style={Style.switchStyle}>
-            <Image source={switchLogo} />
-          </TouchableOpacity>
-          <Text style={[Style.deviceSubtext2 , Style.switchText]}>3hrs ago</Text>
-
-</View>
-    </View>
-    </View>
-       
+        <View>
+          <View style={[Style.rowDirection, Style.switchSWrappertyle]}>
+            <View>
+              <Text style={Style.devicetext}>{active}</Text>
+              <Text style={Style.deviceSubtext2}>Master Switch</Text>
+            </View>
+            <View>
+              <TouchableOpacity style={Style.switchStyle}>
+                <Image source={switchLogo} />
+              </TouchableOpacity>
+            </View>
+          </View>
+        </View>
+        <View style={Style.connectedWrapper}>
+          <Text style={Style.greetingText}>Connected Device</Text>
+          <ScrollView>
+            {switchArray.map((b, i) => {
+              return <Device item={b} key={i} />;
+            })}
+          </ScrollView>
+        </View>
       </View>
       <View style={Style.subView}>
         <View style={Style.sideViewWrapper}>
@@ -65,9 +107,11 @@ const Task = () => {
             return (
               <TouchableOpacity
                 onPress={() => setActive(a.name)}
-                style={Style.sideIcon}
+                style={
+                  active === a.name ? Style.activeStyleIcon : Style.sideIcon
+                }
                 key={i}>
-                <Image source={a.icon} />
+                <Image source={active === a.name ? a.activeIcon : a.icon} />
               </TouchableOpacity>
             );
           })}
